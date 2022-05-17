31bbd79d9a37f6ab61b71af8371a023549f35efa
timjoo <tim@rbtinc.io>
2022-05-16 22:48:08 -0400

    Create server.js
    Commented out for now

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// require('dotenv').config();
+
+// const express = require('express');
+// const cors = require('cors')
+// const app = express();
+// const cookieParser = require('cookie-parser');
+
+
+// app.use(cookieParser());
+// app.use(cors({origin: 'http://localhost:3000', credentials:true }));
+// app.use(express.json());   
+// app.use(express.urlencoded({ extended: true }));
+
+// require('./config/mongoose.config'); 
+// require('./routes/user.routes')(app);
+// require('./routes/comment.routes')(app);
+
+
+// const port = process.env.SERVER_PORT;
+// app.listen(port, () => console.log(`Listening on port: ${port}`) );
