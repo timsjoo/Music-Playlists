ae107137cc918e9712b2ec2c6fbd6852375ab88b
timjoo <tim@rbtinc.io>
2022-05-19 14:48:50 -0400

    Fixed/uncommented server.js, created user and playlist controller/routes

---
+++ server/routes/playlist.route.js
@@ -0,0 +1,10 @@
+const PlaylistController = require('../controllers/playlist.controller');
+const { authenticate } = require('../config/jwt.config');
+
+module.exports = app => {
+  app.get("/api/playlists", PlaylistController.getAllPlaylists);
+  app.get("/api/playlists/:id", PlaylistController.getOnePlaylist);
+  app.post("/api/playlists", authenticate, PlaylistController.createPlaylist);
+  app.put("/api/playlists/:id", PlaylistController.updatePlaylist);
+  app.delete("/api/playlists/:id", PlaylistController.deletePlaylist);
+}
---
+++ server/routes/user.route.js
@@ -0,0 +1,10 @@
+const UserController = require('../controllers/user');
+const {authenticate} = require('../config/jwt.config');
+
+module.exports = app => {
+  app.get("/api/users", authenticate, UserController.getLoggedInUser); // Get object of the single user that is logged in;
+  app.post("/api/users/register", UserController.registerUser); 
+  app.post("/api/users/login", UserController.loginUser);
+  app.post("/api/users/logout", UserController.logoutUser);
+
+}
