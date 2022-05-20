50a7958cb03bf7619a7cada390dc2a043ffda5d1
Yulia Pianova <yuliapianova@Yulias-MacBook-Pro.local>
2022-05-19 17:19:17 -0700

    Login.js, Register.js, App.js, LoginReg.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+//import AllPlaylists from "./components/AllPlaylists";
+//import NewPlaylist from "./components/NewPlaylist";
+//import OnePlaylist from "./components/OnePlaylist";
+//import EditPlaylist from "./components/EditPlaylist";
+import LoginReg from "./views/LoginReg";
+
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 function App() {
+  
+  
   return (
+
+    <BrowserRouter>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+      <Routes>
+        <Route element = {<LoginReg />} path= "/" />
+        
+
+      </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Register.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+
+const Register = (props) => {
+
+    const [confirmReg, setConfirmReg] = useState("");
+    const [errors, setErrors] = useState({});
+
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const register = (e) => {
+        e.preventDefault();
+
+        axios.post("http://localhost:8000/api/users/register", 
+            user,
+
+            {withCredentials: true}
+            )
+            .then((res) => {
+                console.log(res.data);
+                setUser({
+                    username: "",
+                    email: "",
+                    password: "",
+                    confirmPassword: "",
+                });
+                setConfirmReg(
+                    "Thank you for Registering!",
+                );
+                setErrors({});
+            })
+            .catch((err) => {
+                console.log(err);
+                setErrors(err.response.data.errors);
+            })
+
+    }
+
+    return(
+
+        <div>
+
+            <h2>Register</h2>
+
+            {confirmReg ? <h4 style={{ color: "green" }}>{confirmReg}</h4> : null}
+
+            <form onSubmit={register}>
+
+            <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username:</label>
+                {errors.username ? (
+                        <span className="error-text" >
+                            {errors.username.message}
+                        </span>
+                    ) : null}
+                <input type="text" name="username" value={user.username} onChange={(e) => handleChange(e)} className="form-control" />
+            </div>
+
+
+            <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email:</label>
+                    {errors.email ? (
+                        <span className="error-text">{errors.email.message}</span>
+                    ) : null}
+                    <input type="email" name="email" value={user.email} onChange={handleChange} className="form-control" />
+            </div>
+
+
+            <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password:</label>
+                    {errors.password ? (
+                        <span className="error-text">
+                            {errors.password.message}
+                        </span>
+                    ) : null}
+                    <input type="password" name="password" value={user.password} onChange={handleChange} className="form-control" />
+            </div>
+
+
+            <div className="mb-3">
+                    <label htmlFor="confirmPassword" className="form-label">Confirm Password:</label>
+                    {errors.confirmPassword ? (
+                        <span className="error-text">
+                            {errors.confirmPassword.message}
+                        </span>
+                    ) : null}
+                    <input type="password" name="confirmPassword" value={user.confirmPassword} onChange={handleChange} className="form-control" />
+            </div>
+
+            <div className="center">
+                <button className="btn  btn-outline-success" >Register</button>
+            </div>
+
+            </form>
+
+
+
+        </div>
+    )
+}
+
+export default Register;
