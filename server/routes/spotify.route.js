e5f643e31bc08a517bfeb85a7e531399aa1c3c81
timjoo <tim@rbtinc.io>
2022-05-25 13:16:04 -0400

    Added search feature to Playlist
    Added a search bar in OnePlaylist that will allow users to search for and add songs to their playlist

--- server/routes/spotify.route.js
+++ server/routes/spotify.route.js
@@ -2,5 +2,5 @@ const SpotifyController = require('../controllers/spotify.controller');
 
 module.exports = app => {
   app.get("/songs/:query", SpotifyController.getSongsByKeyword);
-  app.get("/song/songId", SpotifyController.getSongById);
+  app.get("/song/:songId", SpotifyController.getSongById);
 }
