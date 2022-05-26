cdb1ccb247918395488fb29a78e0567ecbfc0741
melissaAcheng <melcheng12@gmail.com>
2022-05-26 01:28:54 -0400

    delete songs from playlist

--- client/src/components/TrackSearchResult.js
+++ client/src/components/TrackSearchResult.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const TrackSearchResult = ({track, chooseTrack}) => {
-
+const TrackSearchResult = ({ track, chooseTrack }) => {
   const smallestAlbumImage = track.album.images.reduce((smallest, image) => {
-    if (image.height < smallest.height ) return image
-    return smallest
-  })
+    if (image.height < smallest.height) return image;
+    return smallest;
+  });
 
   const handleAdd = () => {
-    chooseTrack(track)
-  }
+    chooseTrack(track);
+  };
   // console.log(track)
 
   return (
-  <div className="d-flex m-2 align-items-flex-start" style={{cursor: "pointer"}} onClick={handleAdd}>
-    <img src = {smallestAlbumImage.url} style={{height: '64px', width: '64px'}} alt="album" />
-    <div className="ml-3">
-      <div>{track.name}</div>
-      <div>{track.artists[0].name}</div>
+    <div className="d-flex m-2 align-items-flex-start" style={{ cursor: "pointer" }} onClick={handleAdd}>
+      <img src={smallestAlbumImage.url} style={{ height: "64px", width: "64px" }} alt="album" />
+      <div className="ml-3">
+        <div>{track.name}</div>
+        <div>{track.artists[0].name}</div>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default TrackSearchResult; 
+export default TrackSearchResult;
